df7893162638fa47bc0621e56e7018773153ed61
Crowdbotics <team@crowdbotics.com>
2020-09-16 14:17:41 +0000

    Update models

--- backend/omegaelectricnow_20335/wsgi.py
+++ backend/omegaelectricnow_20335/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omegaelectricnow_20335.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omegaelectricnow_20335.settings")
 
 application = get_wsgi_application()
